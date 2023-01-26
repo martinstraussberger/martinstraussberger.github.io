d2ed212985e574f58bc6ec823c08b427d8eb0305
Martin Straußberger <>
2023-01-26 11:29:28 +0100

    gitignore

---
+++ public/js/toggle.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('._themeToggle');
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const darkTheme = document.body.classList.toggle('dark-theme');
+const lightTheme = document.body.classList.toggle('light-theme');
+
+const currentTheme = localStorage.getItem('theme');
+currentTheme ? 'dark' : darkTheme;
+currentTheme ? 'light' : lightTheme;
+
+btn.addEventListener('click', function () {
+  let theme;
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle('light-theme');
+    theme = document.body.classList.contains('light-theme') ? 'light' : 'dark';
+  } else {
+    document.body.classList.toggle('dark-theme');
+    theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+  }
+  localStorage.setItem('theme', theme);
+});
