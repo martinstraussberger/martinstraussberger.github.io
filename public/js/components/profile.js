8ae3cc90ed55cf5b1894e122a393c90e7b1e7c86
Martin Straußberger <>
2023-01-29 11:47:57 +0100

    changed path of qr for github

--- public/js/components/profile.js
+++ public/js/components/profile.js
@@ -23,7 +23,7 @@ const Profile = `<h1>Fullstack | Frontend Engineer</h1>
       <img
         class="icon _linkedIn"
         style="color: #fff"
-        src="/public/images/icons/linkedin.png"
+        src="./public/images/icons/linkedin.png"
         width="43px"
         alt="github"
       />
@@ -31,7 +31,7 @@ const Profile = `<h1>Fullstack | Frontend Engineer</h1>
     <button class="qr-btn">
       <img
         class="icon _qr"
-        src="/public/images/qr-codes/profile-qr.png"
+        src="./public/images/qr-codes/profile-qr.png"
         width="76px"
         alt="github"
       />
