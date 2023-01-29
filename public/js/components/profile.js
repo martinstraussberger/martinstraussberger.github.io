704144ded5764ef1ee88a0ac9485bfe6b283ec1c
Martin Straußberger <>
2023-01-29 12:19:49 +0100

    changed profile qr code image positioning

--- public/js/components/profile.js
+++ public/js/components/profile.js
@@ -18,26 +18,26 @@ const Profile = `<h1>Fullstack | Frontend Engineer</h1>
     </p>
   </div>
   <p>Get in touch</p>
+  <button class="qr-btn">
+    <img
+      class="icon _qr"
+      src="/public/images/qr/profileQr.png"
+      width="76px"
+      alt="github"
+    />
+  </button>
   <div class="flex">
     <a target="_blank" href="https://linkedin.com/in/martinstraussberger">
       <img
         class="icon _linkedIn"
         style="color: #fff"
-        src="./public/images/icons/linkedin.png"
+        src="/public/images/icons/linkedin.png"
         width="43px"
         alt="github"
       />
     </a>
-    <button class="qr-btn">
-      <img
-        class="icon _qr"
-        src="./public/images/qr-codes/profile-qr.png"
-        width="76px"
-        alt="github"
-      />
-    </button>
   </div>`;
 
-  window.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('profile').innerHTML = Profile;
-  });
+window.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('profile').innerHTML = Profile;
+});
