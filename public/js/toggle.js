26959eeacd0fa5e6cd379c01d57a1c4924fdfc4a
Martin Straußberger <>
2023-02-02 16:17:35 +0100

    fixed dark and light mode bug on path switch

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,12 +1,19 @@
 const Toggle = () => {
   const btn = document.querySelector('._themeToggle');
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-  const FifteenDaysOfDark = document.body.classList.toggle('dark-theme');
-  const FifteenDaysOfLight = document.body.classList.toggle('light-theme');
-
   const currentTheme = localStorage.getItem('theme');
-  currentTheme ? 'dark' : FifteenDaysOfDark;
-  currentTheme ? 'light' : FifteenDaysOfLight;
+  const AddFifteenDaysOfDarkness = document.body.classList.add('dark-theme');
+  const AddFifteenDaysOfLight = document.body.classList.add('light-theme');
+
+  if (currentTheme === 'dark') {
+    console.log('index if:', localStorage.getItem('theme'));
+    document.body.classList.remove('light-theme');
+    AddFifteenDaysOfDarkness;
+  } else {
+    console.log('index else if:', localStorage.getItem('theme'));
+    document.body.classList.remove('dark-theme');
+    AddFifteenDaysOfLight;
+  }
 
   btn.addEventListener('click', function () {
     let theme;
