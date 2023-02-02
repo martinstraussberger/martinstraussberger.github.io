569d29c7aa578230b56b731259fde5737bbbf070
Martin Straußberger <>
2023-02-02 11:48:11 +0100

    added timeboxing template pdf and preview image + a personal development section within about page

--- public/js/components/about.js
+++ public/js/components/about.js
@@ -4,6 +4,50 @@ const HomeBtn = `
 </a>
 `;
 
+const PersonalDevelopment = `
+<h1>Shot of Awe how I approach my Goals</h1>
+<p>
+  As a fan of personal development, I firmly believe investing a couple of hours into ourselves is crucial. 
+  After all, it's time to take control of our lives and not let our inner temptations play the game for us. 
+  That's why I've created a Timeboxing Model that's free, accessible, and built with HTML and CSS, 
+  so you too can harness the power of time management. They say time is money, but I say it's worth even more. 
+  Every hour that passes without valuing it is a lost opportunity. An hour lost is not just a missed opportunity 
+  but a depletion of the precious and limited time we have on this unique planet. So, let's make the most of every minute 
+  with the help of the Timeboxing concept. 
+
+  </br>
+
+  I can personally attest to its effectiveness and its positive impact on my daily goals and life aspirations. 
+  For example, building this website and rescuing a lost puppy from Morocco were two monumental tasks while working full-time. 
+  We fought and organized for five months to bring him to Germany, undergo the blood test procedures, and satisfy all government requirements. 
+  So why not give it a shot? 🚀 
+  
+  </br>
+
+  The first challenge is sticking to it, but trust me, it's worth it. After all, the only thing standing between you 
+  and your dreams is the time you choose to invest in yourself." 🙃
+  </br>
+
+  <h4>In short, what is Timeboxing?</h4>
+  <p>
+    Timeboxing is a time management technique where a specific amount of time is allocated for a task or activity. The goal is to increase focus and productivity. 
+    A Timeboxing Model involves setting clear, achievable goals for each cluster and sticking to the designated time frame. A cluster is called Timeslot. 
+    It helps to prioritize tasks and manage time more effectively, reducing the risk of procrastination and distractions. Additionally, regularly reviewing and 
+    adjusting the time allocations can lead to continuous improvement and optimization of the model and your mindset.
+  </p>
+</p>
+`;
+
+const PreviewTimeBoxing = `
+<div class="flex">
+<h4>Preview</h4>
+  <a>
+    <img src="../public/images/PreviewTimeBoxing.jpg" width="290px" height="490px"
+  </a>  
+  <a target="_blank" href="./public/pdf/timeBoxingTemplate.pdf"><b>PDF</b></a>
+</div>
+`
+
 const About = ` 
 <div class="_content flex biography">
     <h2>Bio</h2>
@@ -48,6 +92,9 @@ const About = `
     my skills to your business, accelerating the research and development process from
     scratch to a fully functional Application, beyond just an MVP.
     </p>
+    ${PersonalDevelopment}
+    ${PreviewTimeBoxing}
+    </br>
     ${HomeBtn}
 </div>
 `;
